88d91965b3e83ff78f53bc486b6828dbc19131c6
Ayesha Naveed <hr34772@gmail.com>
2025-11-30 17:10:17 +0500

    system calls

---
+++ syscalls.py
@@ -0,0 +1,15 @@
+from gui import update_gui_log
+from scheduler import run_scheduler
+def sys_write(text):
+    update_gui_log(f"[SYSCALL WRITE] {text}")
+
+
+def sys_exit():
+    update_gui_log("[SYSCALL EXIT] Process ended")
+    run_scheduler()
+
+
+syscalls = {
+    1: lambda: sys_write("Hello from process"),
+    2: sys_exit
+}
